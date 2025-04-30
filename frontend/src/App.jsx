5e6dbc4ebb8b4da51d95cfe145cb5c2fbe4a055d
Gabriel Freitas <gabrielfresan1@gmail.com>
2025-04-30 20:24:38 -0300

    Adição da logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TaskList from "./components/TaskList";
+import todoLogo from "./assets/todo-logo.png"; // Import the image file
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-gray-100">
-        <header className="bg-blue-600 text-white p-4">
-          <div className="container mx-auto">
-            <h1 className="text-xl font-bold">Todo App</h1>
+        <header className="bg-gray-200 text-gray-600 p-4">
+          <div className="container mx-auto flex items-center">
+            <div className="flex items-center">
+              <img
+                src={todoLogo}
+                alt="Todo App Logo"
+                className="h-10 w-10 mr-3"
+              />
+              <h1 className="text-xl font-bold">Todo App</h1>
+            </div>
           </div>
         </header>
 
